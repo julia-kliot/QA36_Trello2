b342fb74b781a279d626f3f03225a2a84fa7d6b8
julia-kliot <juliakliot.jk@gmail.com>
2023-01-12 20:28:31 +0300

    login

---
+++ src/test/java/tests/Login.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Login extends TestBase {
+
+    @BeforeMethod
+    public void preConditions() {
+        if (app.getUser().isLogged()) {
+            app.getUser().logOut();
+        }
+    }
+
+    @Test
+    public void login1() {
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillInLoginForm("juliakliot.jk@gmail.com", "misha240613");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+        Assert.assertTrue(app.getUser().isLogged());
+
+    }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,19 @@
+package tests;
+
+import manage.ApplicationManager;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+    static ApplicationManager app = new ApplicationManager();
+
+    @BeforeSuite(alwaysRun = true)
+    public void  setUp(){
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void stop(){
+        app.quit();
+    }
+}
